ddcdd467e5d896ec8d8137aed972b989274e6cf2
bels <bels11@yahoo.com>
2021-02-07 20:01:02 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <NewItemForm />
+  </div>
+</template>
+
+<script setup>
+  import NewItemForm from './components/NewItemForm.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewItemForm.vue
@@ -0,0 +1,34 @@
+<template>
+	<form>
+		<div class="form-group">
+		  <label for="item-name">Item Name</label>
+		  <input type="text" class="form-control" v-model="itemName">
+		</div>
+		<button type="button" class="btn btn-primary" v-on:click="submit">Create</button>
+	</form>
+</template>
+
+<script setup>
+	axios.get('/locations').then(res =>{
+	
+	}).catch(err => {
+	
+	})
+</script>
+
+<script>
+	export default {
+		data: function(){
+			return {
+				itemName: '',
+				locations: []
+			}
+		},
+		methods: {
+			submit: function(){
+				console.log(this.itemName);
+				this.itemName = '';
+			}
+		}
+	}
+</script>
