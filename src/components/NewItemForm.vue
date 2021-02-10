7b4daa38938d302c7ea189e73985719adf2c6be8
bels <bels11@yahoo.com>
2021-02-09 20:31:00 -0500

    Item type form

--- src/components/NewItemForm.vue
+++ src/components/NewItemForm.vue
@@ -2,18 +2,22 @@
 	<form>
 		<div class="form-group">
 		  <label for="item-name">Item Name</label>
-		  <input type="text" class="form-control" v-model="itemName">
+		  <input type="text" class="form-control" v-model="itemName" />
+		</div>
+		<div class="form-group">
+			<label for="item-type">Item Type</label>
+			<select class="custom-select" v-model="itemType" :options="itemTypes"></select>
 		</div>
 		<button type="button" class="btn btn-primary" v-on:click="submit">Create</button>
 	</form>
 </template>
 
 <script setup>
-	axios.get('/locations').then(res =>{
-	
+	axios.get('http://localhost:3000/item/types').then(res =>{
+		this.itemTypes = res.data;
 	}).catch(err => {
-	
-	})
+		console.log(err);
+	});
 </script>
 
 <script>
---
+++ src/components/NewItemTypeForm.vue
@@ -0,0 +1,18 @@
+<template>
+	<form>
+		<div class="form-group">
+			<label for="item-type-name">Item Type Name</label>
+			<input type="text" class="form-control" v-model="itemTypeName" />
+		</div>
+	</form>
+</template>
+
+<script>
+	export default {
+		data: function(){
+			return {
+				itemTypeName: ''
+			}
+		}
+	}
+</script>
