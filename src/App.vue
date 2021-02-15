b6803f5298666097a2195530d3e4558b0b93b9af
bels <justin@fullstack-solutions.com>
2021-02-15 11:35:19 -0500

    Added tabs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <div class="container">
-    <NewItemForm />
-    <NewItemTypeForm />
+  	<div class="row">
+  		<div class="col">
+  			<h2 class="text-center">Pantry App</h2>
+  		</div>
+  	</div>
+  	<div class="row">
+  		<div class="col">
+  			<ul class="nav nav-tabs">
+  				<li class="nav-item">
+  					<a href="#item-tab-content" id="item-tab-link" role="tab" aria-controls="item-tab-link" data-toggle="tab" aria-selected="true" class="nav-link active">Item</a>
+  				</li>
+  				<li class="nav-item">
+  					<a href="#item-types-tab-content" id="item-types-tab-link" role="tab" aria-controls="item-types-tab-link" data-toggle="tab" aria-selected="true" class="nav-link">Item Types</a>
+  				</li>
+  				<li class="nav-item">
+  					<a href="#item-location-tab-content" id="item-location-tab-link" role="tab" aria-controls="item-locations-tab-link" data-toggle="tab" aria-selected="true" class="nav-link">Item Locations</a>
+  				</li>
+  				<li class="nav-item">
+  					<a href="#recipe-tab-content" id="recipe-tab-link" role="tab" aria-controls="recipe-tab-link" aria-selected="true" data-toggle="tab" class="nav-link">Recipes</a>
+  				</li>
+  			</ul>
+  			<div class="tab-content" id="tab-content">
+  				<div class="tab-pane fade show active" id="item-tab-content" role="tabpanel" aria-labelledby="item-tab-link">
+  					<NewItemForm />
+  				</div>
+  				<div class="tab-pane fade show" id="item-types-tab-content" role="tabpanel" aria-labelledby="item-types-tab-link">
+  					<NewItemTypeForm />
+  				</div>
+  				<div class="tab-pane fade show" id="item-location-tab-content" role="tabpanel" aria-labelledby="item-locations-tab-link">
+  					
+  				</div>
+  				<div class="tab-pane fade show" id="recipe-tab-content" role="tabpanel" aria-labelledby="recipe-tab-link">
+  					
+  				</div>
+  			</div>
+  		</div>
+  	</div>
   </div>
 </template>
 
