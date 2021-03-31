4c0eced00b79a574cf52da92882c78697ca2a45e
bels <bels11@yahoo.com>
2021-03-30 20:06:53 -0400

    Ugly potential recipe items

--- src/components/NewRecipeForm.vue
+++ src/components/NewRecipeForm.vue
@@ -1,7 +1,20 @@
 <template>
-
+	<div>
+		<PotentialRecipeItem v-for="item in potentialItems" :item="item" />
+	</div>
 </template>
 
 <script>
+	import PotentialRecipeItem from './PotentialRecipeItem.vue';
 
+	export default {
+		components: {
+			PotentialRecipeItem: PotentialRecipeItem
+		},
+		computed: {
+			potentialItems() {
+				return this.$store.state.items;
+			}
+		}
+	}
 </script>
---
+++ src/components/PotentialRecipeItem.vue
@@ -0,0 +1,42 @@
+<template>
+	<label>{{item.name}}</label>
+	<input class="form-control" type="checkbox" @click="clickSelected">
+	<input class="form-control" type="text" v-model="amount" />
+	<select v-model="uom" class="custom-select">
+		<option value="ounce">Ounce</option>
+		<option value="dry-cup">Dry Cup</option>
+		<option value="fluid-cup">Fluid Cup</option>
+		<option value="pint">Pint</option>
+		<option value="quart">Quart</option>
+		<option value="teaspoon">Teaspoon</option>
+		<option value="tablespoon">TableSpoon</option>
+	</select>
+	<button type="button" class="btn btn-primary" @click="logEverything">Log It</button>
+</template>
+
+<script>
+
+export default {
+	props: {
+		item: Object
+	},
+	data: function(){
+		return {
+			amount: 0,
+			uom: '',
+			selected: false
+		}
+	},
+	methods: {
+		clickSelected: function(){
+			this.selected = !this.selected;
+		},
+		logEverything: function(){
+			console.log(this.amount);
+			console.log(this.uom);
+			console.log(this.selected);
+		}
+	}
+}
+
+</script>
